736c281ffe26b6b740dd62bdc29a03a634e0cc87
TerserahGw <120995106+TerserahGw@users.noreply.github.com>
2025-06-23 21:37:37 +0700

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext.jsx';
 import { SocketProvider } from './context/SocketContext.jsx';
 import AppRoutes from './routes/AppRoutes.jsx';
+import Navbar from './components/layout/Navbar.jsx';
 
 function App() {
   return (
     <AuthProvider>
       <SocketProvider>
         <Router>
-          <Routes>
-            <Route path="*" element={<AppRoutes />} />
-          </Routes>
+          <Navbar />
+          <div className="pt-16"> {/* Padding untuk navbar */}
+            <AppRoutes />
+          </div>
         </Router>
       </SocketProvider>
     </AuthProvider>
